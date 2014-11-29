6dcd77b89b451a7ccbedbd5975c4e42a50a3f622
codie1991 <codiewestphall@gmail.com>
2014-11-29 14:29:03 +1300

    Initial commit

---
+++ app/src/main/java/com/codiew/lollipop_playground/MyActivity.java
@@ -0,0 +1,16 @@
+package com.codiew.lollipop_playground;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by codiewestphall on 29/11/14.
+ */
+public class MyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
