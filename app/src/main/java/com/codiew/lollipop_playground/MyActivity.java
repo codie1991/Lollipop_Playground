4cd19a810eec016f75e9c7145f7f64e435b1f96e
Codie Westphall <codiewestphall@gmail.com>
2014-11-29 15:41:25 +1300

    Base toolbar setup for backwards compatibility and material design

--- app/src/main/java/com/codiew/lollipop_playground/MyActivity.java
+++ app/src/main/java/com/codiew/lollipop_playground/MyActivity.java
@@ -2,6 +2,7 @@ package com.codiew.lollipop_playground;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by codiewestphall on 29/11/14.
@@ -12,5 +13,7 @@ public class MyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
